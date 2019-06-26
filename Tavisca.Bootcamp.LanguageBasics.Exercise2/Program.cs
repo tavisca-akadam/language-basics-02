df4fe4fd5c963483e542ab12a60123e6553745a2
tavisca-akadam <akadam@tavisca.in>
2019-06-26 17:29:14 +0530

    Review Changes

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -9,7 +9,7 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Test(new[] {"12:12:12"}, new [] { "few seconds ago" }, "12:12:12");
             Test(new[] { "23:23:23", "23:23:23" }, new[] { "59 minutes ago", "59 minutes ago" }, "00:22:23");
             Test(new[] { "00:10:10", "00:10:10" }, new[] { "59 minutes ago", "1 hours ago" }, "impossible");
-            Test(new[] { "11:59:13", "11:13:23", "12:25:15" }, new[] { "few seconds ago", "46 minutes ago", "23 hours ago" }, "11:59:23");
+            Test(new[] { "11:59:13", "12:25:15", "11:13:23" }, new[] { "few seconds ago", "23 hours ago", "46 minutes ago" }, "11:59:23");
             Console.ReadKey(true);
         }
 
@@ -21,67 +21,50 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine($"[{postTimesCsv}], [{showTimesCsv}] => {result}");
         }
 
-        public static bool isinvalid(string[] exactPostTime, string[] showPostTime)
-        {
-            for (int i = 0; i < exactPostTime.Length; i++) 
-            {
-                for (int j = i + 1; j < exactPostTime.Length; j++)
-                {
-                    if (exactPostTime[i] == exactPostTime[j])
-                    {
-                        if (showPostTime[i] != showPostTime[j])
-                            return true;
-                    }
-                }
-            }
-            return false;
-        }
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
             // Add your code here.
-            //check If the information given is self-contradictory
-            if(isinvalid(exactPostTime,showPostTime))
-            {
+            Helper helper = new Helper();
+            if(helper.IsInvalid(exactPostTime, showPostTime))
                 return "impossible";
-            }
 
-            string currentTime = "";
-            DateTime[] timeArray = new DateTime[exactPostTime.Length];
-            int mins = 0, hrs = 0, sec = 0;
-            DateTime calcTime;
-            bool flag = true;
+            DateTime[] postTimeArray = new DateTime[exactPostTime.Length];
+            int minute = 0, hour = 0, second = 0;
+            DateTime calculatedTime;
+
             for(int i = 0; i < exactPostTime.Length; i++)
             {
                 string[] showPostTimeWords = showPostTime[i].Split(' ');
-                timeArray[i] = DateTime.Parse(exactPostTime[i]);
-                if(flag)
-                {   
-                    //initianlize all variables
-                    hrs = timeArray[i].Hour;
-                    mins = timeArray[i].Minute;
-                    sec = timeArray[i].Second;
-                    flag = false;
+                postTimeArray[i] = DateTime.Parse(exactPostTime[i]);
+                
+                if(showPostTimeWords[1].Equals("seconds"))
+                {
+                    hour = postTimeArray[i].Hour;
+                    minute = postTimeArray[i].Minute;
+                    second = postTimeArray[i].Second;
                 }
 
                 //Adding Hours
                 if(showPostTimeWords[1].Equals("hours"))
                 {
-                    hrs = timeArray[i].AddHours(int.Parse(showPostTimeWords[0])).Hour;
+                    int postHour = int.Parse(showPostTimeWords[0]);
+                    calculatedTime = helper.AddHour(postTimeArray[i], postHour);
+                    hour = calculatedTime.Hour;
+                    minute = calculatedTime.Minute;
+                    second = calculatedTime.Second;
                 }
                 //Adding minutes
                 else if(showPostTimeWords[1].Equals("minutes"))
                 {
-                    calcTime = timeArray[i].AddMinutes(int.Parse(showPostTimeWords[0]));
-                    hrs = calcTime.Hour;
-                    mins = calcTime.Minute;
-                    sec = timeArray[i].Second;
+                    int postMinute = int.Parse(showPostTimeWords[0]);
+                    calculatedTime = helper.AddMinute(postTimeArray[i], postMinute);
+                    hour = calculatedTime.Hour;
+                    minute = calculatedTime.Minute;
+                    second = calculatedTime.Second;
                 }
             }
-            string temp = "" + hrs;
-            if(hrs == 0) temp = "00"; 
-            //calculated time to string
-            currentTime = $"{temp}:{mins}:{sec}";
-            return currentTime;
+            return helper.ToString(hour, minute, second);
+
             throw new NotImplementedException();
         }
     }
